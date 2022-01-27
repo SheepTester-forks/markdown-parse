469fdc3920b93be5b7be0e7826f65c376a1f4629
Sean <ussa.say@gmail.com>
2022-01-27 13:42:38 -0800

    Write a test for `getLinks` Must run using javac/java commands

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,9 +1,21 @@
 import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
 import org.junit.*;
 
 public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void testGetLinks() throws IOException {
+        String markdown = Files.readString(Path.of("test-file.md"));
+        assertEquals(List.of("https://something.com", "some-page.html"), MarkdownParse.getLinks(markdown));
     }
 }
