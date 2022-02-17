f293cd5d14e48c7f6d00779ba4f6a70c7f588a7b
Sean <ussa.say@gmail.com>
2022-02-16 17:17:47 -0800

    add my tests

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,6 +1,7 @@
 import static org.junit.Assert.*;
 import org.junit.*;
 import java.util.ArrayList;
+import java.util.List;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.io.IOException;
@@ -93,5 +94,71 @@ public class MarkdownParseTest {
         ArrayList<String> links = MarkdownParse.getLinks(contents);
         assertEquals(linkTester, links);
         System.out.println("test change 2");
+    }
+
+    @Test
+    public void testBasicLink() throws IOException {
+        String markdown = Files.readString(Path.of("basic-link.md"));
+        assertEquals(List.of("https://sheeptester.github.io/"), MarkdownParse.getLinks(markdown));
+    }
+
+    @Test
+    public void testEmpty() throws IOException {
+        String markdown = Files.readString(Path.of("empty.md"));
+        assertEquals(List.of(), MarkdownParse.getLinks(markdown));
+    }
+
+    @Test
+    public void testEscaping() throws IOException {
+        String markdown = Files.readString(Path.of("escaping.md"));
+        assertEquals(List.of("hi2.html"), MarkdownParse.getLinks(markdown));
+    }
+
+    @Test
+    public void testImage() throws IOException {
+        String markdown = Files.readString(Path.of("image.md"));
+        assertEquals(List.of(), MarkdownParse.getLinks(markdown));
+    }
+
+    @Test
+    public void testSquareBracketGang() throws IOException {
+        String markdown = Files.readString(Path.of("square-bracket-gang.md"));
+        assertEquals(List.of("before", "after"), MarkdownParse.getLinks(markdown));
+    }
+
+    @Test
+    public void testUnclosedParens() throws IOException {
+        String markdown = Files.readString(Path.of("unclosed-parens.md"));
+        assertEquals(List.of("test", "test?test[=3", "ok"), MarkdownParse.getLinks(markdown));
+    }
+
+    @Test
+    public void testWeirdUrls() throws IOException {
+        String markdown = Files.readString(Path.of("weird-urls.md"));
+        assertEquals(List.of("https://en.wikipedia.org/wiki/1984_(disambiguation)"), MarkdownParse.getLinks(markdown));
+    }
+
+    @Test
+    public void testImage2() throws IOException {
+        String markdown = Files.readString(Path.of("image2.md"));
+        assertEquals(List.of("google.com"), MarkdownParse.getLinks(markdown));
+    }
+
+    @Test
+    public void testIncorrect() throws IOException {
+        String markdown = Files.readString(Path.of("incorrect.md"));
+        assertEquals(List.of(), MarkdownParse.getLinks(markdown));
+    }
+
+    @Test
+    public void testNewFile() throws IOException {
+        String markdown = Files.readString(Path.of("new-file.md"));
+        assertEquals(List.of("google.com", "some-()()([][][][])()()page().html"), MarkdownParse.getLinks(markdown));
+    }
+
+    @Test
+    public void testFailureInducingInput() throws IOException {
+        String markdown = Files.readString(Path.of("failure-inducing-input.md"));
+        assertEquals(List.of(""), MarkdownParse.getLinks(markdown));
     }
 }
