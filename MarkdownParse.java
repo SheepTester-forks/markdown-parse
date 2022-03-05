90cdcf257cdaea4bbcfa98ba57095e63461c8999
Sean <ussa.say@gmail.com>
2022-03-05 13:14:47 -0800

    Make MarkdownParse CLI support directory

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -1,9 +1,13 @@
 
 // File reading code from https://howtodoinjava.com/java/io/java-read-file-to-string-examples/
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MarkdownParse {
     public static ArrayList<String> getLinks(String markdown) {
@@ -84,10 +88,34 @@ public class MarkdownParse {
         return toReturn;
     }
 
+    public static Map<String, List<String>> getLinks(File dirOrFile) throws IOException {
+        Map<String, List<String>> result = new HashMap<>();
+        if (dirOrFile.isDirectory()) {
+            for (File f : dirOrFile.listFiles()) {
+                result.putAll(getLinks(f));
+            }
+            return result;
+        } else {
+            Path p = dirOrFile.toPath();
+            int lastDot = p.toString().lastIndexOf(".");
+            if (lastDot == -1 || !p.toString().substring(lastDot).equals(".md")) {
+                return result;
+            }
+            ArrayList<String> links = getLinks(Files.readString(p));
+            result.put(dirOrFile.getPath(), links);
+            return result;
+        }
+    }
+
     public static void main(String[] args) throws IOException {
-        Path fileName = Path.of(args[0]);
-        String contents = Files.readString(fileName);
-        ArrayList<String> links = getLinks(contents);
-        System.out.println(links);
+        File fileOrDir = new File(args[0]);
+        if (fileOrDir.isDirectory()) {
+            System.out.println(getLinks(fileOrDir));
+        } else {
+            Path fileName = Path.of(args[0]);
+            String contents = Files.readString(fileName);
+            ArrayList<String> links = getLinks(contents);
+            System.out.println(links);
+        }
     }
 }
